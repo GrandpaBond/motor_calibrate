8b99afa1db493d0d31ad4ec7cb7f15d57922d258
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2021-10-11 18:07:10 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,43 @@
-let Bias = 0
+input.onButtonPressed(Button.A, function () {
+    if (Signed_Bias < 10) {
+        Signed_Bias += 1
+        basic.showNumber(Signed_Bias)
+    }
+})
+function flash_speed () {
+    for (let index = 0; index < 2; index++) {
+        basic.showNumber(speed)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+    basic.pause(3000)
+}
+input.onButtonPressed(Button.B, function () {
+    if (Signed_Bias > -10) {
+        Signed_Bias += -1
+        basic.showNumber(Signed_Bias)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    speed += -10
+    flash_speed()
+    Signed_Bias = 0
+})
+let speed = 0
+let Signed_Bias = 0
+music.setVolume(0)
+Signed_Bias = 0
+speed = 100
+flash_speed()
+basic.showNumber(Signed_Bias)
 basic.forever(function () {
-	
+    if (Signed_Bias < 0) {
+        Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Left, 0 - Signed_Bias)
+    } else {
+        Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, Signed_Bias)
+    }
+    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+    basic.pause(5000)
+    Kitronik_Move_Motor.stop()
 })
